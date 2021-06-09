39f365d9c767808659aa14906a1d52daa22cb72e
Jesse Reitz <jessereitz1@gmail.com>
2021-06-09 09:19:10 -0500

    Cleanup and styled-components
    * Clean up some create-react-app boilerplate
    * Start playing around with styled-components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import styled, { css } from 'styled-components'
+
+const Button = styled.button`
+  background: transparent;
+  border-radius: 3px;
+  border: 2px solid palevioletred;
+  color: palevioletred;
+  margin: 0 1em;
+  padding: 0.25em 1em;
+  transition: 0.2s all;
+
+  &:hover {
+    background: palevioletred;
+    color: white;
+  }
+
+  ${props =>
+    props.primary &&
+    css`
+      background: palevioletred;
+      color: white;
+
+      &:hover {
+        background: white;
+        color: palevioletred;
+      }
+    `
+  }
+`
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <Button onClick={() => alert('heyo!')}>
+          Press Me!
+        </Button>
+        <Button primary onClick={() => alert('heyo!')}>
+          No, press Me!
+        </Button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
